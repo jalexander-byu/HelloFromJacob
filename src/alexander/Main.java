2d469940fa39a72cdff859b0b86c4d63d9c99d6e
jalexander-byu <77984474+jalexander-byu@users.noreply.github.com>
2021-01-30 21:05:56 -0700

    Update Main.java

--- src/alexander/Main.java
+++ src/alexander/Main.java
@@ -3,9 +3,8 @@ package alexander;
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("Hello, my name is Jacob.");
+	System.out.println("Hello, my name is Jacob.");
         System.out.println("You have been hacked by Mathew.");
         System.out.println("Second Matthew reporting in.");
-
     }
 }
